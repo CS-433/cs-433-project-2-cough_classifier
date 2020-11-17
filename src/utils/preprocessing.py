30a6197e60b8f8df999c9f507fdca91e5215d8ee
Xavi Oliva <xavi.oliva6@gmail.com>
2020-11-17 10:55:42 +0100

    refactor previous commit

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -2,33 +2,45 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
-def import_data(path, segmentation_type):
+def import_data(path, segmentation_type, is_user_features=True):
     """
     Import data
     :param path: path of data
     :type path: str
     :param segmentation_type: 'no', 'coarse', or 'fine'
     :type segmentation_type: str
+    :param is_user_features: specify if user features should be dropped
     :return: dataframes containing features and labels
     """
     df_features = pd.read_csv(f'{path}/features_{segmentation_type}_segmentation.csv', index_col=0)
     df_labels = pd.read_csv(f'{path}/labels_{segmentation_type}_segmentation.csv', index_col=0)
-    df_features = df_features.set_index("File_Name")
-    df_labels = df_labels.set_index("File_Name")
+
+    if segmentation_type in ('fine', 'coarse'):
+        df_features = create_multi_index(df_features)
+        df_labels = create_multi_index(df_labels)
+    else:
+        df_features.set_index("File_Name")
+        df_features.rename(index={'File_Name': 'subject'})
+        df_labels.set_index("File_Name")
+        df_labels.rename(index={'File_Name': 'subject'})
+
+    drop_features = ['Expert']
+    df_features.drop(drop_features, axis=1, errors='ignore', inplace=True)
+
+    if not is_user_features:
+        user_features = ['Age', 'Gender', 'Resp_Condition', 'Symptoms']
+        df_features.drop(user_features, axis=1, errors='ignore', inplace=True)
 
     return df_features, df_labels
 
 
-def create_multi_index(data, labels):
-    data["subject"] = data.index.map(lambda r: r.split("_")[0])
-    data["file_id"] = data.index.map(lambda r: r.split("_")[1])
+def create_multi_index(data):
+    data["subject"] = data["File_Name"].apply(lambda r: r.split("_")[0])
+    data["file_id"] = data["File_Name"].apply(lambda r: r.split("_")[1])
     data.set_index(['subject', 'file_id'], inplace=True)
-    
-    labels["subject"] = labels.index.map(lambda r: r.split("_")[0])
-    labels["file_id"] = labels.index.map(lambda r: r.split("_")[1])
-    labels.set_index(['subject', 'file_id'], inplace=True)
-    
-    return data, labels
+    data.drop(["File_Name"], axis=1, errors='ignore', inplace=True)
+
+    return data
 
 
 def standardize(df):
